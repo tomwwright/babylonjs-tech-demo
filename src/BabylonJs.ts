f58309f14ddbe840ddcfe26d9436294eb0268309
Tom Wright <tom.suffern.wright@gmail.com>
2024-01-17 19:51:56 +1100

    fix: clean up spheres

--- src/BabylonJs.ts
+++ src/BabylonJs.ts
@@ -7,7 +7,6 @@ import {
   HemisphericLight,
   MeshBuilder,
   ActionManager,
-  SetValueAction,
   Color3,
   StandardMaterial,
   ExecuteCodeAction,
@@ -60,83 +59,20 @@ export const initialiseBabylonJs = ({
   camera,
   scene,
 }: InitialiseBabylonJsProps) => {
+  // configure camera controls
+
   camera.attachControl(canvas, true);
 
-  camera.radius = 3;
-  camera.upperRadiusLimit = 5;
+  camera.radius = 5;
+  camera.upperRadiusLimit = 10;
   camera.lowerRadiusLimit = 1.5;
 
+  camera.alpha = Math.PI;
+
   camera.mapPanning = true;
-  camera.lowerBetaLimit = Math.PI / 3;
-  camera.upperBetaLimit = Math.PI / 3;
-
-  new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-
-  const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-  const material = new StandardMaterial("material", scene);
-  material.diffuseColor = new Color3(0.4, 0.4, 0.4);
-  material.specularColor = new Color3(0.4, 0.4, 0.4);
-  material.emissiveColor = Color3.Purple();
-  sphere.material = material;
-
-  const updateCursor = (event: MouseEvent) => {
-    setCursor({
-      x: event.clientX,
-      y: event.clientY,
-      active: true,
-    });
-  };
-
-  sphere.actionManager = new ActionManager(scene);
-  sphere.actionManager.registerAction(
-    new SetValueAction(
-      ActionManager.OnPointerOverTrigger,
-      sphere.material,
-      "emissiveColor",
-      Color3.White()
-    )
-  );
-
-  const onPointerOver = () => {
-    console.log("pointer over");
-    window.addEventListener("mousemove", updateCursor);
-  };
-
-  sphere.actionManager.registerAction(
-    new ExecuteCodeAction(ActionManager.OnPointerOverTrigger, onPointerOver)
-  );
-
-  sphere.actionManager.registerAction(
-    new SetValueAction(
-      ActionManager.OnPointerOutTrigger,
-      sphere.material,
-      "emissiveColor",
-      material.emissiveColor
-    )
-  );
-
-  const onPointerOut = () => {
-    console.log("pointer out");
-    window.removeEventListener("mousemove", updateCursor);
-    setCursor({
-      x: 0,
-      y: 0,
-      active: false,
-    });
-  };
-
-  sphere.actionManager.registerAction(
-    new ExecuteCodeAction(ActionManager.OnPointerOutTrigger, onPointerOut)
-  );
-
-  stateObservable.add((state) => {
-    console.log("from babylon", state);
-    if (state.isOn) {
-      material.alpha = 1;
-    } else {
-      material.alpha = 0;
-    }
-  });
+  const cameraAngleDegrees = (Math.PI / 180) * 45;
+  camera.lowerBetaLimit = cameraAngleDegrees;
+  camera.upperBetaLimit = cameraAngleDegrees;
 
   // camera rotation
 
@@ -200,7 +136,34 @@ export const initialiseBabylonJs = ({
     }
   });
 
-  // set up scene objects
+  // hovering for tooltip
+
+  const updateCursor = (event: MouseEvent) => {
+    setCursor({
+      x: event.clientX,
+      y: event.clientY,
+      active: true,
+    });
+  };
+
+  const onPointerOver = () => {
+    console.log("pointer over");
+    window.addEventListener("mousemove", updateCursor);
+  };
+
+  const onPointerOut = () => {
+    console.log("pointer out");
+    window.removeEventListener("mousemove", updateCursor);
+    setCursor({
+      x: 0,
+      y: 0,
+      active: false,
+    });
+  };
+
+  // set up map of hexagons
+
+  new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
   const radius = 1;
   const spacingX = radius * 1.1 * 1.5;
@@ -278,41 +241,15 @@ export const initialiseBabylonJs = ({
   scene.removeMesh(template);
   template.dispose();
 
-  // spheres
+  // demo reacting to state changes from react
 
-  const sphere2 = MeshBuilder.CreateSphere("sphere2", { diameter: 0.5 }, scene);
-  sphere2.position.x = 1.5;
-  sphere2.position.z = -1;
-  const material2 = new StandardMaterial("material2", scene);
-  material2.diffuseColor = new Color3(0.4, 0.4, 0.4);
-  material2.specularColor = new Color3(0.4, 0.4, 0.4);
-  material2.emissiveColor = Color3.Green();
-  sphere2.material = material2;
-
-  sphere2.actionManager = new ActionManager(scene);
-  sphere2.actionManager.registerAction(
-    new SetValueAction(
-      ActionManager.OnPointerOverTrigger,
-      sphere2.material,
-      "emissiveColor",
-      Color3.White()
-    )
-  );
-
-  sphere2.actionManager.registerAction(
-    new ExecuteCodeAction(ActionManager.OnPointerOverTrigger, onPointerOver)
-  );
-
-  sphere2.actionManager.registerAction(
-    new SetValueAction(
-      ActionManager.OnPointerOutTrigger,
-      sphere2.material,
-      "emissiveColor",
-      material2.emissiveColor
-    )
-  );
-
-  sphere2.actionManager.registerAction(
-    new ExecuteCodeAction(ActionManager.OnPointerOutTrigger, onPointerOut)
-  );
+  stateObservable.add((state) => {
+    console.log("from babylon", state);
+    const material = map[0][0].material as StandardMaterial;
+    if (state.isOn) {
+      material.alpha = 1;
+    } else {
+      material.alpha = 0;
+    }
+  });
 };
