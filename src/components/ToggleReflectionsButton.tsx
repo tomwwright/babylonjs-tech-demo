84b98c12ab23da968eda5446aaa06fd5896b95cb
Tom Wright <tom.suffern.wright@gmail.com>
2024-07-13 12:26:20 +1000

    refactor: simplify with partial update

--- src/components/ToggleReflectionsButton.tsx
+++ src/components/ToggleReflectionsButton.tsx
@@ -1,18 +1,17 @@
 import { useSceneState } from "../SceneState";
 
 export const ToggleReflectionsButton = () => {
-  const state = useSceneState();
+  const { reflectionsEnabled, setState } = useSceneState();
 
   const toggleOnOff = () => {
-    state.setState({
-      ...state,
-      reflectionsEnabled: !state.reflectionsEnabled,
+    setState({
+      reflectionsEnabled: !reflectionsEnabled,
     });
   };
 
   return (
     <button style={{ pointerEvents: "auto" }} onClick={toggleOnOff}>
-      Reflections ({state.reflectionsEnabled ? "on" : "off"})
+      Reflections ({reflectionsEnabled ? "on" : "off"})
     </button>
   );
 };
